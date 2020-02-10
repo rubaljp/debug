e53119ffcf6d0d718e83c0f33fda2d27a37382d3
jatinder174@gmail.com <rubaljpsjp>
2020-02-09 20:48:05 -0500

    commit by jatinder singh with the new text fields and buttons for login page with static data

--- app/src/main/java/com/example/thiiddd/MainActivity.java
+++ app/src/main/java/com/example/thiiddd/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.thiiddd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView view =findViewById(R.id.editText);
+    Intent intent =new Intent(MainActivity.this,SignUp.class);
     }
 }
